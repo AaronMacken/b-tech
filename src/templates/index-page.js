c9aa04bdcbce41f81e58614700f83f5b1064eebd
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-12 12:19:56 -0500

    Set up the skeleton structure for the contact section of the landing page and the footer. Add css for these sections in all.css

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/Layout";
 import ImageCard from "../components/ImageCard";
+import kyle from "./kyle.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const IndexPageTemplate = ({ image, title, subheading, services }) => {
   const { card1, card2, card3 } = services;
@@ -37,6 +40,45 @@ export const IndexPageTemplate = ({ image, title, subheading, services }) => {
           <ImageCard image={card2.image} card_title={card2.card_title} text={card2.card_text} />
           <ImageCard image={card3.image} card_title={card3.card_title} text={card3.card_text} />
         </div>
+      </section>
+      <section className="landing-about">
+        <h2>Who We Are</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <img src={kyle} alt="Kyle Lmaoo" />
+            </div>
+            <div className="col-md-5">
+              <p>
+                <FontAwesomeIcon icon={faQuoteLeft} />
+                We are very happy to work with Altar.io. The collaboration goes far beyond a
+                standard IT service: with Altar.io we have a sparring partner that challenges us in
+                our product decisions and is truly passionate about the results developed. The level
+                of professionalism and dedication is exceptional in this industry and we are eager
+                to strengthen our collaboration in the future.
+              </p>
+              <h4>Liam Morison</h4>
+              <h5>Place Logo Here</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="landing-form">
+        <h2>Hit us up</h2>
+        <form className="form-wrapper">
+          <input type="text" placeholder="name" className="form-input" />
+          <input type="email" placeholder="email" className="form-input" />
+          <textarea
+            name="message"
+            placeholder="message"
+            cols="30"
+            rows="10"
+            className="form-input"
+          ></textarea>
+          <button type="submit" className="button">
+            Send Message
+          </button>
+        </form>
       </section>
     </>
   );
