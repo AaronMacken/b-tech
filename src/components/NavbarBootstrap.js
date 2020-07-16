aa5720a757f1b975d42c55c740d53c3c20f60f9c
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-16 08:10:58 -0500

    Add gatsby links on navbar.

--- src/components/NavbarBootstrap.js
+++ src/components/NavbarBootstrap.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "gatsby";
 
 class NavbarBootstrap extends Component {
   // state taht will be triggered when someone scrolls down from the top of the page
@@ -70,15 +70,15 @@ class NavbarBootstrap extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-center">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Link className="navbar-item" to="/">
+              Home
+            </Link>
+            <Link className="navbar-item" to="/contact">
+              Contact
+            </Link>
+            <Link className="navbar-item" to="/blog">
+              F.A.Q.
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
