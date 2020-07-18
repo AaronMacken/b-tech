8919d300e71e52adc1cd3aba530bbe8a8325cd45
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-18 10:29:19 -0500

    add skeleton html & css for the contact thanks page

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,11 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from "react";
+import Layout from "../../components/Layout";
 
 export default () => (
-  <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
-        </div>
-      </div>
-    </section>
+  <Layout flat>
+    <div className="contact-thanks">
+      <h1>Thanks for reaching out!</h1>
+      <p>Someone will be in contact with you typically within 24 hours.</p>
+    </div>
   </Layout>
-)
+);
