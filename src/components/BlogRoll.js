bc9aba353656863a55d7458ec20c515bbbaeb60a
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-19 13:30:01 -0500

    Set up return cards for the blog roll component

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,60 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
 class BlogRoll extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <div className="columns is-multiline">
+      <div className="faqcard-wrapper">
         {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
+          posts.map(({ node: post }) => {
+            return (
+              <div className="faqcard">
+                <div className="card-body2">
+                  <h4 className="card-title font-weight-bold">{post.frontmatter.title}</h4>
+                  <p className="card-text">{post.excerpt}</p>
+                  <div className="align-right">
+                    <Link to={post.fields.slug} className="btn btn-primary">
+                      Read More
                     </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </div>
-    )
+    );
   }
 }
 
@@ -64,7 +37,7 @@ BlogRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default () => (
   <StaticQuery
@@ -76,7 +49,7 @@ export default () => (
         ) {
           edges {
             node {
-              excerpt(pruneLength: 400)
+              excerpt(pruneLength: 100)
               id
               fields {
                 slug
@@ -101,4 +74,46 @@ export default () => (
     `}
     render={(data, count) => <BlogRoll data={data} count={count} />}
   />
-)
+);
+
+// ORIGINAL DATA RETURNED
+// <div className="is-parent column is-6" key={post.id}>
+//   <article
+//     className={`blog-list-item tile is-child box notification ${
+//       post.frontmatter.featuredpost ? 'is-featured' : ''
+//     }`}
+//   >
+//     <header>
+//       {post.frontmatter.featuredimage ? (
+//         <div className="featured-thumbnail">
+//           <PreviewCompatibleImage
+//             imageInfo={{
+//               image: post.frontmatter.featuredimage,
+//               alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+//             }}
+//           />
+//         </div>
+//       ) : null}
+//       <p className="post-meta">
+//         <Link
+//           className="title has-text-primary is-size-4"
+//           to={post.fields.slug}
+//         >
+//           {post.frontmatter.title}
+//         </Link>
+//         <span> &bull; </span>
+//         <span className="subtitle is-size-5 is-block">
+//           {post.frontmatter.date}
+//         </span>
+//       </p>
+//     </header>
+//     <p>
+//       {post.excerpt}
+//       <br />
+//       <br />
+//       <Link className="button" to={post.fields.slug}>
+//         Keep Reading →
+//       </Link>
+//     </p>
+//   </article>
+// </div>
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -14,32 +14,8 @@ export default class BlogIndexPage extends React.Component {
             <h1 className="faqtitle2">We got answers</h1>
           </div>
 
-          {/* CARD WRAPPER */}
-          <div className="faqcard-wrapper">
-            {/* CARD */}
-            <div className="faqcard">
-              <div className="card-body2">
-                <h4 className="card-title font-weight-bold">
-                  Just in: small batch of Jamaican Blue Mountain in store next week
-                </h4>
-                <p className="card-text">
-                  We’re proud to announce that we’ll be offering a small batch of Jamaica Blue
-                  Mountain coffee beans in our store next week.{" "}
-                </p>
-                <div className="align-right">
-                  <button className="btn btn-primary">Read More</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
-          </div>
-        </section> */}
+          {/* CARDS */}
+          <BlogRoll />
         </section>
       </Layout>
     );
