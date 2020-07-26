94ba79857bf2ca5d4e308755e343b32ab4f41e5d
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-26 14:23:54 -0500

    Finish about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,23 +3,25 @@ import PropTypes from "prop-types";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaQuoteLeft } from "react-icons/fa";
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
     <main className="about-page-section">
+      {/* TEXT */}
       <section className="about-page-header-wrapper">
         <h1 className="title-dark about-titles" style={{ marginBottom: "0" }}>
           About
         </h1>
         <h1 className="title-light about-titles">Us</h1>
       </section>
+      {/* SERVICES */}
       <section className="about-page-white about-page-service-section">
         <div className="about-us-service-section-text">
           <p>"Babylon Technologies is all about bringing powerful resources to small businesses"</p>
-          <Link className="about-page-services-link" href="#services">
+          <Link className="about-page-services-link" to="/services">
             Our Services <FaArrowRight />
           </Link>
         </div>
@@ -29,9 +31,54 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
           className="about-page-service-section-img shadow"
         />
       </section>
-      <section className="about-page-white">
-        <h2>{title}</h2>
-        <PageContent content={content} />
+      {/* BELEIFS */}
+      <section className="about-page-beleifs-section">
+        <h2 className="title-dark section-heading" id="about-page-heading">
+          Here is what we believe
+        </h2>
+        <div className="belief-card-contaier">
+          <div className="card shadow">
+            <img src="/img/mission.png" alt="vision icon" className="belief-icon" />
+            <div className="card-body">
+              <h4 className="card-title-b">Our Mission</h4>
+              <hr />
+              <p>
+                <FaQuoteLeft style={{ marginRight: "1rem" }} />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci{" "}
+              </p>
+            </div>
+          </div>
+          <div className="card shadow">
+            <img src="/img/vision.png" alt="vision icon" className="belief-icon" />
+            <div className="card-body">
+              <h4 className="card-title-b">Our Vision</h4>
+              <hr />
+              <p>
+                <FaQuoteLeft style={{ marginRight: "1rem" }} />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci{" "}
+              </p>
+            </div>
+          </div>
+          <div className="card shadow">
+            <img src="/img/values.png" alt="vision icon" className="belief-icon" />
+            <div className="card-body">
+              <h4 className="card-title-b">Our Values</h4>
+              <hr />
+              <p>
+                <FaQuoteLeft style={{ marginRight: "1rem" }} />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="about-page-white about-page-content">
+        <h2 className="title-light section-heading" id="about-page-heading">
+          {title}
+        </h2>
+        <div className="container content">
+          <PageContent content={content} />
+        </div>
       </section>
     </main>
   );
