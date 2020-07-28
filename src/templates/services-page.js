f6554e3ad386fd90bf91f0320e69764239837e0c
Aaron Macken <aaron.a.macken@gmail.com>
2020-07-27 19:50:21 -0500

    Make static version of services page

--- src/templates/services-page.js
+++ src/templates/services-page.js
@@ -2,15 +2,74 @@ import React from "react";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import { FaEnvelope } from "react-icons/fa";
 
 export const ServicesPageTemplate = () => {
   return (
     <main className="about-page-section">
       <section className="about-page-header-wrapper">
         <h1 className="title-dark about-titles" style={{ marginBottom: "0" }}>
-          Services
+          Our
         </h1>
-        <h1 className="title-light about-titles">Us</h1>
+        <h1 className="title-light about-titles">Services</h1>
+      </section>
+      <section className="services-content">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 services-content-wrapper">
+              <div className="service-box">
+                <img src="/img/serv1.jpeg" alt="some stuff" className="shadow" />
+
+                <div className="service-box-text">
+                  <h2>Something we do</h2>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facilis cum
+                    eveniet quam. Architecto voluptatum quas natus ut pariatur cupiditate
+                    voluptatem, fugit a ullam laboriosam ex hic dolores distinctio dolore debitis
+                    reprehenderit in at maiores eum dolorum nihil soluta eos repellat rem? Deserunt
+                    praesentium sunt beatae quia maiores delectus provident!
+                  </p>
+                  <Link className="btn btn-primary btn-lg text-white" to="/contact">
+                    Contact Us <FaEnvelope />
+                  </Link>
+                </div>
+              </div>
+              <div className="service-box">
+                <div className="service-box-text">
+                  <h2>Something we do</h2>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facilis cum
+                    eveniet quam. Architecto voluptatum quas natus ut pariatur cupiditate
+                    voluptatem, fugit a ullam laboriosam ex hic dolores distinctio dolore debitis
+                    reprehenderit in at maiores eum dolorum nihil soluta eos repellat rem? Deserunt
+                    praesentium sunt beatae quia maiores delectus provident!
+                  </p>
+                  <Link className="btn btn-primary btn-lg text-white" to="/contact">
+                    Contact Us <FaEnvelope />
+                  </Link>
+                </div>
+                <img src="/img/serv2.jpeg" alt="some stuff" className="shadow" />
+              </div>
+              <div className="service-box">
+                <img src="/img/serv3.jpeg" alt="some stuff" className="shadow" />
+
+                <div className="service-box-text">
+                  <h2>Something we do</h2>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facilis cum
+                    eveniet quam. Architecto voluptatum quas natus ut pariatur cupiditate
+                    voluptatem, fugit a ullam laboriosam ex hic dolores distinctio dolore debitis
+                    reprehenderit in at maiores eum dolorum nihil soluta eos repellat rem? Deserunt
+                    praesentium sunt beatae quia maiores delectus provident!
+                  </p>
+                  <Link className="btn btn-primary btn-lg text-white" to="/contact">
+                    Contact Us <FaEnvelope />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
